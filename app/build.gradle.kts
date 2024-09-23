26655ac7e3dccb487f36a1ca6aeace637fb93be5
phamHoang04 <hoangzzz75@gmail.com>
2024-09-23 20:59:35 +0700

    Update Logo va thu vien

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+///////
+    implementation ("androidx.compose.material:material-icons-extended:1.6.7")
+    implementation("com.google.android.material:material:1.12.0")
+
+    // thư viện chuyển màn hình
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
