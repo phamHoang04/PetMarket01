8ccb3b8c15f658e94487db39923032d263eda7d9
phamHoang04 <hoangzzz75@gmail.com>
2024-09-30 22:55:57 +0700

    Update man thanhtoan, thanhcong, cart, thongbao

---
+++ app/src/main/java/com/example/petmarket/Activity/ThanhToanActivity.kt
@@ -0,0 +1,14 @@
+package com.example.petmarket.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.petmarket.R
+
+class ThanhToanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_thanhtoan)
+
+    }
+}
