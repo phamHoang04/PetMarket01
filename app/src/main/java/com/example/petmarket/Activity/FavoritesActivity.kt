f2c08f4421e9c23301611ba5cede1db70b6a35c5
phamHoang04 <hoangzzz75@gmail.com>
2024-10-04 21:38:11 +0700

    update

--- app/src/main/java/com/example/petmarket/Activity/FavoritesActivity.kt
+++ app/src/main/java/com/example/petmarket/Activity/FavoritesActivity.kt
@@ -12,10 +12,10 @@ class FavoritesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_favorites)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
